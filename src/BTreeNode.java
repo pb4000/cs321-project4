ca10902a3cd4f631ab36dbd2e3744c34d99aa5b7
Phillip <pb4000231@gmail.com>
2020-04-21 17:48:24 -0700

    Quick update

--- src/BTreeNode.java
+++ src/BTreeNode.java
@@ -1,13 +1,15 @@
 
 public class BTreeNode implements Comparable<BTreeNode> {
     private int[] childPointers;
-    String[] values;
+    // store values as numbers converted from binary
+    // to convert back, convert value to binary, then fill in preceeding 0s based on k
+    Long[] values;
     private int selfPointer;
     private int parentPointer;
 
     public BTreeNode(int k, int selfPointer, int parentPointer) {
         childPointers = new int[k];
-        values = new String[k];
+        values = new Long[k];
         this.selfPointer = selfPointer;
         this.parentPointer = parentPointer;
         
@@ -17,7 +19,7 @@ public class BTreeNode implements Comparable<BTreeNode> {
         return -1;
     }
 
-    public String[] getValues() {
+    public Long[] getValues() {
         return values;
     }
 
@@ -32,7 +34,9 @@ public class BTreeNode implements Comparable<BTreeNode> {
     public int getTotalObjects() {
         int totalObjects = 0;
         for (int i = 0; i < values.length; i++) {
-            if (!values[i].equals(""))
+            // what if stored value is 000000? use size k to infer values stored, but how to discern empty vs 000000?
+            // default array filled with -1?
+            if (values[i] != 0L)
                 totalObjects++;
         }
         return totalObjects;
@@ -58,5 +62,13 @@ public class BTreeNode implements Comparable<BTreeNode> {
         output += parentPointer + "\n";
         output += getTotalObjects() + "\n";
         return output;
+    }
+
+    private String valueToString(Long input) {
+        String output = "";
+        String binary = Long.toBinaryString(input);
+        for (int i = binary.length(); i < values.length; i++)
+            output += "0";
+        return output + binary;
     }
 }
--- src/Testing.java
+++ src/Testing.java
@@ -6,6 +6,14 @@ public class Testing {
 
         Long[] list = new Long[1];
         list[0] = 0b010001L;
-        System.out.println(Long.toBinaryString(list[0]));
+        System.out.println(Long.valueOf(list[0]));
+
+        Long test = Long.parseLong("010001", 2);
+        String output = "";
+        String binary = Long.toBinaryString(test);
+        for (int i = binary.length(); i < 8; i++)
+            output += "0";
+        output += binary;
+        System.out.println(output);
     }
 }
