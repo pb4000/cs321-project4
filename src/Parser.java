d460afcf5aa8c3a8e85b1136bcfee60af3cd8c38
Wesley Brown <wesleybrown792@u.boisestate.edu>
2020-04-21 19:06:50 -0600

    check

--- src/Parser.java
+++ src/Parser.java
@@ -3,10 +3,14 @@ public class Parser {
     public Parser(String X)
 
         // instantiates a ScannerWrapper and reads file into a BTreeNode object
-        public static BTreeNode fileToNode(int lineNumber);
+        public static BTreeNode fileToNode(int lineNumber){
+
+        }
     
         // calls BTreeNode.toString
-        public static String nodeToFile(BTreeNode node);
+        public static String nodeToFile(BTreeNode node){
+
+        }
     
         // converts binary long to String of DNA
         public static String binaryToDNA(long bin){
@@ -15,6 +19,6 @@ public class Parser {
     
         // converts String DNA to a binary long
         public static long dnaToBinary(String dna){
-
+            //sad
         }
 }
