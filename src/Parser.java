25bc79654b381eff4ec2d30fc2260c811ba48011
Wesley Brown <wesleybrown792@u.boisestate.edu>
2020-04-21 19:02:20 -0600

    Created Parser class

--- src/Parser.java
+++ src/Parser.java
@@ -1,3 +1,20 @@
-public interface Parser {
+public class Parser {
+
+    public Parser(String X)
+
+        // instantiates a ScannerWrapper and reads file into a BTreeNode object
+        public static BTreeNode fileToNode(int lineNumber);
     
+        // calls BTreeNode.toString
+        public static String nodeToFile(BTreeNode node);
+    
+        // converts binary long to String of DNA
+        public static String binaryToDNA(long bin){
+
+        }
+    
+        // converts String DNA to a binary long
+        public static long dnaToBinary(String dna){
+
+        }
 }
