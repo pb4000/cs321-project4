8d17c7f0ab0364203517d58b129dc619a4b1742e
Wesley Brown <wesleybrown792@u.boisestate.edu>
2020-05-06 15:34:23 -0600

    stage 2

--- src/GeneBankCreateBTree.java
+++ src/GeneBankCreateBTree.java
@@ -6,8 +6,7 @@ public class GeneBankCreateBTree {
     String[] args;
     boolean usingCache;
     CacheDriver<BTreeNode> cache;
-    File btreeFile;
-    File queryFile;
+    File gbkfile;
     int debugLevel;
     BTreeNode root;
     ScannerWrapper scan;
@@ -27,26 +26,11 @@ public class GeneBankCreateBTree {
 
     public void run() {
         parseArgs();
-        initVars();
-    }
-
-
-    public void initVars() {
-        // get metadata for the overall tree
-        int[] meta = ScannerWrapper.getMetadata(btreeFile);
-        k = meta[0];
-        degree = meta[1];
-        // init root node
-        scan = new ScannerWrapper(btreeFile, degree, k);
-        root = scan.getNode(meta[2]);
-        if (usingCache) {
-            cache.add(root);
-        }
     }
 
     public void parseArgs() {
         // check for correct length
-        if (args.length < 3 || args.length > 5) {
+        if (args.length < 4 || args.length > 6) {
             printUsage();
             System.exit(1);
         }
@@ -60,33 +44,34 @@ public class GeneBankCreateBTree {
             usingCache = true;
             cache = new CacheDriver<BTreeNode>(1, Integer.parseInt(args[3]));
             // set debug level if it exists
-            if (args.length == 5) {
-                debugLevel = Integer.parseInt(args[4]);
+            if (args.length == 6) {
+                debugLevel = Integer.parseInt(args[5]);
             }
         } else {
             // set debug level if it exists
-            if (args.length == 4) {
-                debugLevel = Integer.parseInt(args[3]);
+            if (args.length == 5) {
+                debugLevel = Integer.parseInt(args[4]);
             }
         }
         // initialize BTree file
-        btreeFile = new File(args[1]);
-        if (!btreeFile.exists()) {
-            FileNotFoundException e = new FileNotFoundException("Given BTree file does not exist.");
-            e.printStackTrace();
-            System.exit(1);
-        }
+        degree = Integer.parseInt(args[1]);
+        k = Integer.parseInt(args[3]);
+        root=null;
         // initialize query file
-        queryFile = new File(args[2]);
-        if (!queryFile.exists()) {
-            FileNotFoundException e = new FileNotFoundException("Given query file does not exist.");
+        gbkfile = new File(args[2]);
+        if (!gbkfile.exists()) {
+            FileNotFoundException e = new FileNotFoundException("Given gbk file does not exist.");
             e.printStackTrace();
             System.exit(1);
         }
     }
 
+    public void readGBK(){
+
+    }
+
     public static void printUsage() {
         System.out.println(
-                "Usage:\njava GeneBankSearch <0/1(no/with Cache)> <btree file> <query file> [<cache size>] [<debug level>]");
+                "Usage:\njava GeneBankCreateBTree <0/1(no/with Cache)> <btree file> <query file> [<cache size>] [<debug level>]");
     }
 }
